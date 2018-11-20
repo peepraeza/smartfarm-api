20b710a45bcf5b58200d0e205f2947621b49e500
Peerawit-SC <peerawit.sc@gmail.com>
2018-11-21 01:22:37 +0700

    edit file

--- server.php
+++ server.php
@@ -8,20 +8,38 @@ $config->setAuthConfigFile(__DIR__.'google-service-account.json');
 
 $firebase = new Firebase('https://smart-farm-27e2b.firebaseio.com/', $config);
 
-$content = file_get_contents("php://input");
-// $firebase->getReference('data/qq')
-//    ->set([
-//        'DHT22' => 32,
-//        'emails' => 'peerawit.sc',
-//        'website' => 'https://app.domain.tld'
-//       ]);
-$firebase->push([
-    'key_1' => 'value_1',
-    'key_2' => 'value_2',
-    ], 'data');
-// $myfile = fopen("newfile" . $i . ".txt", "w") or die("Unable to open file!");
+date_default_timezone_set("Asia/Bangkok"); // php://input
+$obj = json_decode(file_get_contents("php://input"), true);
+$time = date("h:i:sa");
+$data = [
+    'valve_0' => [
+        'status' => $obj['valve_0']['status']
+    ],
+    'o_node_0' => [
+        'air_temperature' => $obj['o_node_0']['air_temperature'],
+        'air_humidity' => $obj['o_node_0']['air_humidity']
+    ],
+    'node_0' => [
+        'air_temperature' => $obj['node_0']['air_temperature'],
+        'air_humidity' => $obj['node_0']['air_humidity'],
+        'soil_moisure' => $obj['node_0']['soil_moisure'],
+        'soil_temperature' => $obj['node_0']['soil_temperature']
+    ],
+    'node_1' => [
+        'air_temperature' => $obj['node_1']['air_temperature'],
+        'air_humidity' => $obj['node_1']['air_humidity'],
+        'soil_moisure' => $obj['node_1']['soil_moisure'],
+        'soil_temperature' => $obj['node_1']['soil_temperature']
+    ],
+    'node_2' => [
+        'air_temperature' => $obj['node_2']['air_temperature'],
+        'air_humidity' => $obj['node_2']['air_humidity'],
+        'soil_moisure' => $obj['node_2']['soil_moisure'],
+        'soil_temperature' => $obj['node_2']['soil_temperature']
+    ],
+    'time' => $time
+];
 
-// $txt = $content;
-// fwrite($myfile, $txt);
-// fclose($myfile);
+$firebase->push($data, 'data');
+
 ?>
