493d5288f6f807359e83d041c0187734e6925a76
Peerawit-SC <peerawit.sc@gmail.com>
2018-12-18 11:41:52 +0700

    add api status

--- api_valve_control.php
+++ api_valve_control.php
@@ -8,24 +8,9 @@ $config->setAuthConfigFile(__DIR__.'google-service-account.json');
 
 $firebase = new Firebase('https://smart-farm-27e2b.firebaseio.com/', $config);
 
-echo "ok";
-// $obj = json_decode(file_get_contents("php://input"), true);
-// $table = $obj['node'];
+echo "ok1";
 
-// $data = [
-//     'data' => [
-//         'air_temperature' => $obj['data']['air_temperature'],
-//         'air_humidity' => $obj['data']['air_humidity'],
-//         'soil_moisure' => $obj['data']['soil_moisure'],
-//         'soil_temperature' => $obj['data']['soil_temperature']
-//     ],
-//     'time' => $obj['time']
-// ];
 
-// $firebase->push($data, $table);
-
-// $update_data = ['Valve1' => 'on']
-
-// $firebase->update($update_data, 'Valve_Status');
+$firebase->update(['Valve1' => 'on'], 'Valve_Status');
 ?>
 
